1f29532d5da9e680a6d5aad045d9600bd894c5c1
Takuma <>
2023-10-09 23:40:27 +0900

    リファクタリング

--- popup.js
+++ popup.js
@@ -1,3 +1,22 @@
-document.getElementById("toggle").addEventListener("click", function () {
-  chrome.runtime.sendMessage({ action: "toggle" });
+const closePopup = () => {
+  setTimeout(() => {
+    window.close();
+  }, 700);
+};
+
+const sendMessageToActiveTab = () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    chrome.tabs.sendMessage(tabs[0].id, {}, (res) => {
+      closePopup();
+    });
+  });
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleButton = document.getElementById("toggle");
+  toggleButton.addEventListener("click", () => {
+    chrome.runtime.sendMessage({ action: "toggle" });
+  });
+
+  sendMessageToActiveTab();
 });
